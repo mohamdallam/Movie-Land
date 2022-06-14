9cd06227c215dc9ebc6f7a01e494b32184fa9e30
mohamdallam <mohamedahmedallam1@gmail.com>
2022-06-14 15:24:07 +0200

    Init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
-import React from "react";
-import ReactDOM from "react";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+///////////////////////////////////////
+
+// import { createRoot } from "react-dom/client";
+// const container = document.getElementById("root");
+// const root = createRoot(container);
+// createRoot(container!) if you use TypeScript
+// root.render(<App tab="home" />);
+
+///////////////////////////////////////
+
+// import App from "./App";
+// import { createRoot } from "react-dom/client";
+// const container = document.getElementById("root");
+// const root = createRoot(container);
+// root.render(<App />);
+
+///////////////////////////////////////
 
 import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+import { createRoot } from "react-dom/client";
+createRoot(document.getElementById("root")).render(<App />);
